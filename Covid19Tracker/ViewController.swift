e282c82eec6a221057ff89f4fb8946ede84c0de7
Sebastian Herman <sebastian.herman@live.com>
2020-04-20 21:32:02 +0200

    Initial Commit

---
+++ Covid19Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid19Tracker
+//
+//  Created by Sebastian Herman on 4/20/20.
+//  Copyright © 2020 Sebastian Herman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
